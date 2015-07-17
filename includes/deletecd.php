964ad67ece5e2a487231069b248921e26088dbd8
pierre thery <pierre.thery@gmail.com>
2015-07-18 00:08:05 +0200

    oups

--- includes/deletecd.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-    require_once('db_connect.php');
-    $id=$_SESSION['card'];
-    $sql = "DELETE FROM base WHERE id_album = $id";
-    $bdd->exec($sql);
-    header("Location: ../index.php?state=list.php");
-    ?>
