765210a63e33414606bcaa7f7af191e4cffe3c7f
pierre thery <pierre.thery@gmail.com>
2016-11-04 23:18:44 +0100

    Version 0.2

--- cdbase.sql
+++ cdbase.sql
@@ -1,16 +1,35 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.2
+-- http://www.phpmyadmin.net
+--
+-- Client :  localhost
+-- Généré le :  Ven 04 Novembre 2016 à 23:15
+-- Version du serveur :  10.1.16-MariaDB
+-- Version de PHP :  5.6.24
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Base de données :  `cdbase`
+--
 
-CREATE TABLE IF NOT EXISTS `albums` (
-  `id` smallint(6) NOT NULL AUTO_INCREMENT,
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `albums`
+--
+
+CREATE TABLE `albums` (
+  `id` smallint(6) NOT NULL,
   `brainz_album` varchar(40) CHARACTER SET latin1 NOT NULL,
-  `band_id` smallint(5) unsigned NOT NULL,
+  `band_id` smallint(5) UNSIGNED NOT NULL,
   `title` varchar(80) CHARACTER SET latin1 NOT NULL,
   `year` varchar(12) CHARACTER SET latin1 NOT NULL,
   `nb_tracks` tinyint(4) NOT NULL,
@@ -18,30 +37,113 @@ CREATE TABLE IF NOT EXISTS `albums` (
   `barcode` varchar(40) CHARACTER SET latin1 DEFAULT NULL,
   `format` varchar(20) CHARACTER SET latin1 DEFAULT NULL,
   `media` tinyint(4) NOT NULL,
-  `genre` varchar(40) CHARACTER SET latin1 NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=150 ;
+  `genre` varchar(40) CHARACTER SET latin1 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE IF NOT EXISTS `bands` (
-  `id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `bands`
+--
+
+CREATE TABLE `bands` (
+  `id` smallint(3) UNSIGNED NOT NULL,
   `brainz_band` varchar(30) NOT NULL,
   `name` varchar(45) NOT NULL,
   `country` varchar(30) NOT NULL,
   `area` varchar(50) DEFAULT NULL,
-  `yearbegin` varchar(4) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=77 ;
+  `yearbegin` varchar(4) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE IF NOT EXISTS `tracks` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `tracks`
+--
+
+CREATE TABLE `tracks` (
+  `id` int(11) NOT NULL,
   `album_id` int(11) NOT NULL,
-  `id_track` tinyint(3) unsigned NOT NULL,
-  `ncd` tinyint(2) unsigned DEFAULT NULL,
+  `id_track` tinyint(3) UNSIGNED NOT NULL,
+  `ncd` tinyint(2) UNSIGNED DEFAULT NULL,
   `title` varchar(45) DEFAULT NULL,
-  `duration` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1234 ;
+  `duration` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(8) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(40) NOT NULL,
+  `email` varchar(60) NOT NULL,
+  `status` varchar(8) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `email`, `status`) VALUES
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin@mydomain.com', 'active');
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `albums`
+--
+ALTER TABLE `albums`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `bands`
+--
+ALTER TABLE `bands`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `tracks`
+--
+ALTER TABLE `tracks`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `albums`
+--
+ALTER TABLE `albums`
+  MODIFY `id` smallint(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
+--
+-- AUTO_INCREMENT pour la table `bands`
+--
+ALTER TABLE `bands`
+  MODIFY `id` smallint(3) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=100;
+--
+-- AUTO_INCREMENT pour la table `tracks`
+--
+ALTER TABLE `tracks`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=696;
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
