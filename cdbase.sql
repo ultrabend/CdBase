9f9c745094745a473eb744db00e8231e9ff70fbc
pierre thery <pierre.thery@gmail.com>
2017-02-10 15:44:08 +0100

    sql update

--- cdbase.sql
+++ cdbase.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost
--- Généré le :  Ven 04 Novembre 2016 à 23:15
+-- Généré le :  Ven 10 Février 2017 à 15:43
 -- Version du serveur :  10.1.16-MariaDB
 -- Version de PHP :  5.6.24
 
@@ -29,15 +29,18 @@ SET time_zone = "+00:00";
 CREATE TABLE `albums` (
   `id` smallint(6) NOT NULL,
   `brainz_album` varchar(40) CHARACTER SET latin1 NOT NULL,
+  `discogs_id` int(16) NOT NULL,
   `band_id` smallint(5) UNSIGNED NOT NULL,
   `title` varchar(80) CHARACTER SET latin1 NOT NULL,
   `year` varchar(12) CHARACTER SET latin1 NOT NULL,
+  `released` varchar(16) NOT NULL,
   `nb_tracks` tinyint(4) NOT NULL,
   `label` varchar(30) CHARACTER SET latin1 NOT NULL,
   `barcode` varchar(40) CHARACTER SET latin1 DEFAULT NULL,
   `format` varchar(20) CHARACTER SET latin1 DEFAULT NULL,
   `media` tinyint(4) NOT NULL,
-  `genre` varchar(40) CHARACTER SET latin1 NOT NULL
+  `genre` varchar(40) CHARACTER SET latin1 NOT NULL,
+  `country` varchar(32) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -49,10 +52,14 @@ CREATE TABLE `albums` (
 CREATE TABLE `bands` (
   `id` smallint(3) UNSIGNED NOT NULL,
   `brainz_band` varchar(30) NOT NULL,
+  `discogs_id` int(16) NOT NULL,
   `name` varchar(45) NOT NULL,
+  `profile` text NOT NULL,
   `country` varchar(30) NOT NULL,
   `area` varchar(50) DEFAULT NULL,
-  `yearbegin` varchar(4) DEFAULT NULL
+  `yearbegin` varchar(4) DEFAULT NULL,
+  `members` text NOT NULL,
+  `urls` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -64,12 +71,11 @@ CREATE TABLE `bands` (
 CREATE TABLE `tracks` (
   `id` int(11) NOT NULL,
   `album_id` int(11) NOT NULL,
-  `id_track` tinyint(3) UNSIGNED NOT NULL,
+  `id_track` varchar(6) NOT NULL,
   `ncd` tinyint(2) UNSIGNED DEFAULT NULL,
   `title` varchar(45) DEFAULT NULL,
-  `duration` int(11) DEFAULT NULL
+  `duration` varchar(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 
 -- --------------------------------------------------------
 
@@ -82,15 +88,9 @@ CREATE TABLE `users` (
   `username` varchar(30) NOT NULL,
   `password` varchar(40) NOT NULL,
   `email` varchar(60) NOT NULL,
-  `status` varchar(8) NOT NULL
+  `status` varchar(8) NOT NULL,
+  `language` varchar(5) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Contenu de la table `users`
---
-
-INSERT INTO `users` (`id`, `username`, `password`, `email`, `status`) VALUES
-(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin@mydomain.com', 'active');
 
 --
 -- Index pour les tables exportées
@@ -128,17 +128,17 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT pour la table `albums`
 --
 ALTER TABLE `albums`
-  MODIFY `id` smallint(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
+  MODIFY `id` smallint(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=93;
 --
 -- AUTO_INCREMENT pour la table `bands`
 --
 ALTER TABLE `bands`
-  MODIFY `id` smallint(3) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=100;
+  MODIFY `id` smallint(3) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=128;
 --
 -- AUTO_INCREMENT pour la table `tracks`
 --
 ALTER TABLE `tracks`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=696;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1130;
 --
 -- AUTO_INCREMENT pour la table `users`
 --
